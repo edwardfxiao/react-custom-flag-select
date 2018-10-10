6066e15f58fb059997013719b690f3160bda9df0
Edward Xiao <edwardfhsiao@gmail.com>
2018-10-10 16:11:55 +0800

    no message

--- example/index.js
+++ example/index.js
@@ -1,3 +1,6 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
