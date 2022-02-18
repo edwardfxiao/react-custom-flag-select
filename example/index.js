ed81bb95073fa9f5502b76a9d6b864ac61159ad0
edwardxiao <edwardfhsiao@gmail.com>
2022-02-18 14:59:15 +0800

    no message

--- example/index.js
+++ example/index.js
@@ -2,156 +2,13 @@ import 'raf/polyfill';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import Markdown from 'react-markdown';
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
 import { Textbox } from 'react-inputs-validation';
 import validator from 'validator';
 import 'react-inputs-validation/lib/react-inputs-validation.min.css';
 import ReactCustomFlagSelect from '../src/js/component/index.ts';
 
 import STYLES from '../src/css/example.css';
-
-const markdownExample = `
-\`\`\`javascript
-import ReactCustomFlagSelect from 'react-custom-flag-select';
-import "react-custom-flag-select/lib/react-custom-flag-select.min.css";
-import { Textbox } from 'react-inputs-validation';
-import 'react-inputs-validation/lib/react-inputs-validation.min.css';
-
-const FLAG_SELECTOR_OPTION_LIST = [
-  { id: '1', name: 'US', displayText: 'US(1)', locale: 'en-US', flag: require('../src/image/flags/us.svg') },
-  { id: '86', name: '中国', displayText: '中国(86)', locale: 'zh-CN', flag: require('../src/image/flags/cn.svg') }
-];
-
-const DEFAULT_AREA_CODE = FLAG_SELECTOR_OPTION_LIST[0].id;
-
-const find = (arr, obj) => {
-  const res = [];
-  arr.forEach(o => {
-    let match = false;
-    Object.keys(obj).forEach(i => {
-      if (obj[i] == o[i]) {
-        match = true;
-      }
-    });
-    if (match) {
-      res.push(o);
-    }
-  });
-  return res;
-};
-
-class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { areaCode: DEFAULT_AREA_CODE, phone: '', hasPhoneError: true, validate: false };
-
-...
-
-  render() {
-    const { areaCode, phone, validate } = this.state;
-    const currentItem = find(FLAG_SELECTOR_OPTION_LIST, { id: areaCode })[0];
-    return (
-
-          ...
-
-            <div style={{ position: 'relative' }}>
-              <div style={{ position: 'absolute', left: '0', top: '10px' }}>
-                <ReactCustomFlagSelect
-                  attributesWrapper={{ id: 'areaCodeWrapper', tabIndex: '1' }} //Optional.[Object].Modify wrapper general attributes.
-                  attributesButton={{ id: 'areaCodeButton' }} //Optional.[Object].Modify button general attributes.
-                  attributesInput={{ id: 'areaCode', name: 'areaCode' }} //Optional.[Object].Modify hidden input general attributes.
-                  value={currentItem.id} //
-                  disabled={false} //
-                  showSearch={true} // Optional.[Bool].Default: false. Show a search box in order to find option quickly.
-                  fields={['name', 'locale']} // Optional.[array].Default: ['name']. Fields for search filtering.
-                  // keyword={''} // Optional.[String].Default: ''. Show a keyword for search box.
-                  showArrow={true} //
-                  animate={true} //Optional.[Bool].Default: false.
-                  optionList={FLAG_SELECTOR_OPTION_LIST} //
-                  // selectOptionListItemHtml={<div>us</div>}//
-                  classNameWrapper={''} //
-                  classNameContainer={''} //
-                  classNameOptionListItem={''} //
-                  classNameOptionListContainer={''} //
-                  classNameDropdownIconOptionListItem={''} //
-                  customStyleWrapper={{}} //
-                  customStyleContainer={{ border: 'none', fontSize: '12px' }} //Optional.[Object].Default: {}.
-                  customStyleSelect={{ width: '100px' }} //Optional.[Object].Default: {}.
-                  customStyleOptionListItem={{}} //Optional.[Object].Default: {}.
-                  customStyleOptionListContainer={{ maxHeight: '100px', overflow: 'auto', width: '120px', marginTop: '11px' }} //Optional.[Object].Default: {}.
-                  onChange={areaCode => {
-                    this.setState({ areaCode: areaCode }, () => {
-                      this.handlePhoneChange(phone);
-                    });
-                  }}
-                />
-              </div>
-              <Textbox
-                tabIndex="1"
-                id="phone"
-                customStyleWrapper={{ height: '100%' }}
-                customStyleContainer={{ height: '100%' }}
-                customStyleInput={{
-                  paddingTop: '0',
-                  paddingBottom: '0',
-                  height: '45px',
-                  paddingLeft: '90px',
-                  paddingRight: '20px'
-                }}
-                value={phone}
-                validate={validate}
-                validationCallback={res =>
-                  this.setState({
-                    hasPhoneError: res,
-                    validate: false
-                  })
-                }
-                type="text"
-                placeholder="Please enter your phone number"
-                onChange={res => {
-                  this.handlePhoneChange(res);
-                }}
-                onBlur={() => {}}
-                validationOption={{
-                  check: true,
-                  required: true,
-                  customFunc: phone => {
-                    if (validator.isMobilePhone(areaCode + phone, currentItem.locale)) {
-                      return true;
-                    } else {
-                      return 'Invalid phone format for ' + currentItem.locale;
-                    }
-                  }
-                }}
-              />
-            </div>
-
-
-...
-
-
-\`\`\`
-`;
-
-const CodeBlock = ({ literal, language }) => {
-  var html = Prism.highlight(literal, Prism.languages[language]);
-  var cls = 'language-' + language;
-
-  return (
-    <pre className={cls}>
-      <code dangerouslySetInnerHTML={{ __html: html }} className={cls} />
-    </pre>
-  );
-};
-
-CodeBlock.propTypes = {
-  literal: PropTypes.string,
-  language: PropTypes.string,
-};
 
 const FLAG_SELECTOR_OPTION_LIST = [
   { id: '1', name: 'US', displayText: 'US(1)', locale: 'en-US', flag: require('../src/image/flags/us.svg') },
@@ -217,7 +74,7 @@ class Index extends Component {
                   value={currentItem.id} //Optional.[String].Default: "".
                   disabled={false} //Optional.[Bool].Default: false.
                   showSearch={true} // Optional.[Bool].Default: false. Show a search box in order to find option quickly.
-                  fields={['name', 'locale']} // Optional.[array].Default: ['name']. Fields for search filtering.
+                  fields={['name', 'locale', 'displayText']} // Optional.[array].Default: ['name']. Fields for search filtering.
                   // keyword={''} // Optional.[String].Default: ''. Show a keyword for search box.
                   showArrow={true} //Optional.[Bool].Default: true.
                   animate={true} //Optional.[Bool].Default: false.
